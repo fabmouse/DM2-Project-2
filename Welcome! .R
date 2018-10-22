9e3623c949f00d5f579a981b32b762f74fdbdae9
Brooke Ann Franklin <fab.mouse@hotmail.com>
2018-10-22 20:20:56 +0100

    Just to make sure everything is working :)

---
+++ Welcome! .R
@@ -0,0 +1,7 @@
+#Hi everyone:
+
+#Please add your name and then commit this document to Git. 
+#Just to make sure it's all working properly... 
+#I'll delete it before we submit our project.
+
+#1. Brooke :) 
