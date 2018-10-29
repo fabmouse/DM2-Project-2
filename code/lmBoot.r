2499f50e969874f2fd7fea2306629dd141d3772d
Ziqi Su <ziqsu43@gmail.com>
2018-10-29 11:24:55 +0000

    just first version: terrible need prove

--- code/lmBoot.r
+++ code/lmBoot.r
@@ -1,13 +1,18 @@
 lmBoot <- function(inputData, nBoot){
   
+  bootResults <- matrix(nrow = 2,ncol = 2* nBoot)
+  
   for(i in 1:nBoot){
     
     # resample our data with replacement
     bootData <- inputData[sample(1:nrow(inputData), nrow(inputData), replace = T),]
     
     # fit the model under this alternative reality
-    bootLM <- lm(y ~ x, data = bootData)
     
+    
+    bootLM <- lm(inputData$Oxygen ~ inputData$Age, data = bootData)
+  
+    bootResults[i] <- coef(bootLM)
     # store the coefs
     if(i == 1){
       
@@ -24,4 +29,15 @@ lmBoot <- function(inputData, nBoot){
   
   bootResults
   
+}
+
+lmBootSu <- function(inputData, nBoot){
+  bootResults <- matrix(nrow = nBoot, ncol = 2)
+  for(i in 1:nBoot){
+    bootData <- inputData[sample(1:nrow(inputData), nrow(inputData), replace = T),]
+    bootLM <- lm(inputData$Oxygen ~ inputData$Age, data = bootData)
+    bootResults[i] <- coef(bootLM)
+    
+  }
+  bootResults
 }
