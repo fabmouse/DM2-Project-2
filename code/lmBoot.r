470176b5de38e5f049fc6d7093f30dd264f13d12
Ziqi Su <ziqsu43@gmail.com>
2018-11-03 15:22:55 +0000

    just commit what I have done really early

--- code/lmBoot.r
+++ code/lmBoot.r
@@ -12,6 +12,7 @@ lmBoot <- function(inputData, nBoot){
     
     # resample our data with replacement
     bootData <- inputData[sample(1:nrow(inputData), nrow(inputData), replace = T),]
+    print(bootData)
     
     # fit the model under this alternative reality
     bootLM <- lm(y ~ x, data = bootData)
@@ -34,8 +35,9 @@ lmBoot <- function(inputData, nBoot){
   
 }
 
-boot <- function(inputData){
-  NumofDim <- norw(inputData)
+get.bootdata <- function(index, inputData,NumofDim){
+  bootData <- inputData[sample(1:NumofDim,NumofDim,replace = T)]
+  #bootLM <- lm(y ~ x, bootData)
   
 }
 
@@ -48,17 +50,23 @@ lmBootSu <- function(inputData, nBoot){
   #bootResults <- matrix(NA,nrow = nBoot, ncol = 2)
   NumofDim <- nrow(inputData)
   print(NumofDim)
-  lapply(1:nBoot , function(y){bootData[y] <- inputData[sample(1:NumofDim, NumofDim, replace = T),]})
-  print(bootData)
+  dataset <- sapply(1:nBoot , function(x){inputData[sample(1:nrow(inputData), nrow(inputData), replace = T),]})
+  print(dataset)
+  print(typeof(dataset))
+  print(dataset[1])
+
+
+  bootLM <- sapply(1:nBoot, function(z){lm( y ~ x, data = dataset)})
+  print(bootLM)
   
-  for(i in 1:nBoot){
+  #for(i in 1:nBoot){
     #bootData <- inputData[sample(1:nrow(inputData), nrow(inputData), replace = T),]
     #print(bootData)
     #bootLM <- lm(y ~ x, data = inputData[sample(1:nrow(inputData), nrow(inputData), replace = T),])
     #bootResults[i,] <- coef(bootLM)
-    bootResults[i,] <- coef(lm(y ~ x, data = inputData[sample(1:nrow(inputData), nrow(inputData), replace = T),]))
+    #bootResults[i,] <- coef(lm(y ~ x, data = inputData[sample(1:nrow(inputData), nrow(inputData), replace = T),]))
     
-  }
+  #}
   
   #boot[i,] <- coef(lm(y ~ x, data = inputData[sample(1:nrow(inputData), nrow(inputData), replace = T),]))
   #bootData <- apply
@@ -66,5 +74,23 @@ lmBootSu <- function(inputData, nBoot){
   #bootResults <- apply(inputData,bootResults,boot)
   #bootResult <- apply(1:nBoot, function(x){ coef(lm(y ~ z, data = x[sample(1:nrow(x), nrow(x), replace = T),])) })
   bootResults
+}
+
+
+
+
+lmBootNew<- function(nBoot, inputData){
+  bootresult <- parApply(1:nBoot, helper, regData = inputData)
+  print(bootResults)
+}
+
+helper <- function(index, regData){
+  dataDim <- nrow(regData)
+  
+  bootData <- regData[sample(1:dataDim, dataDim, replace = T),]
+  
+  bootLM <- lm(y ~ x, bootData)
+  
+  coef(bootLM)
 }
 
