b0c7b1019ca67fb210a98b823377d6fd1241e4d8
Ziqi Su <ziqsu43@gmail.com>
2018-11-03 19:55:46 +0000

    commit microbench.R again

--- code/microbench.R
+++ code/microbench.R
@@ -3,11 +3,10 @@ library(boot)
 #source("lmboot_attempt2.R")
 set.seed(5763)
 
-<<<<<<< HEAD
+
 microbenchmark(lmBoot_4(fitData,10000),times = 10L)
-=======
-microbenchmark(lmBoot_4(fitData,5),times = 10L)
->>>>>>> 8d367b8263d79e6deb3fcbc430b200b50600e1f0
+
+
 
 bst <- function(formula, data, indices){
   d <- data[indices, ]
@@ -17,8 +16,8 @@ bst <- function(formula, data, indices){
 
 
 
-<<<<<<< HEAD
+
 microbenchmark(boot(data = fitData, statistic = bst, R =10000, formula = Oxygen~.),times = 10L)
-=======
-microbenchmark(boot(data = fitData, statistic = bst, R =5, formula = Oxygen~.),times = 10L)
->>>>>>> 8d367b8263d79e6deb3fcbc430b200b50600e1f0
+
+
+
